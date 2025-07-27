77d736c5016694c258c1f6426c4270796f4815e2
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-27 14:58:14 +0200

    updated the data types file

--- day-02-data-types/DataTypes/src/DataTypes.java
+++ day-02-data-types/DataTypes/src/DataTypes.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
 public class DataTypes {
     public static void main(String[] args){
-        System.out.println("Hello datatypes");
+        Scanner input = new Scanner(System.in);
+        
+        Double output;
+
+        System.out.println("------ Welcome To My First Mini Calculator🍀 -------");
+        System.out.print("Enter your first Number: ");
+        Double firstNumber = input.nextDouble();
+        System.out.print("Choose an operator(+, -, *, x): ");
+        char operator = input.next().charAt(0);
+        System.out.print("Enter your second number: ");
+        Double secondNumber = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                output = firstNumber + secondNumber;
+                System.out.println("Result: " + output);
+                break;
+            case '-':
+                output = firstNumber - secondNumber;
+                System.out.println("Result: " + output);
+                break;
+            case '/':
+                if (secondNumber != 0){
+                    output = firstNumber / secondNumber;
+                    System.out.println("Result: " + output);
+                } else {
+                    System.out.println("We cannot devide by 0");
+                }
+                break;
+            case '*':
+                output = firstNumber * secondNumber;
+                System.out.println("Result: " + output);
+                break;
+            default:
+                System.out.println("❌ Invalid operator!");
+                break;
+        }
+
+        
+
+        input.close();
     }
 }
