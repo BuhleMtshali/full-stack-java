20cd41afcb00f9afd10ca6c723f9a5b496bf948f
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-28 16:26:03 +0200

    created the mini area calculator

--- day-03-operators/Operators/src/AreaCalculator.java
+++ day-03-operators/Operators/src/AreaCalculator.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;;
+
 public class AreaCalculator {
     public static void main(String[] args){
-        System.out.println("hello");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("====== Welcome To Mini Area Calculator🍄 =======");
+
+        System.out.println("Enter the name of the shape we are trying calculate: ");
+        String shapeName = input.nextLine();
+
+        System.out.println("Enter the length (cm): ");
+        Double length = input.nextDouble();
+
+        System.out.println("Enter the width (cm): ");
+        Double width = input.nextDouble();
+
+        Double areaTotal = length * width;
+
+        System.out.println("The Area of the " + shapeName + " is: " + areaTotal + "cm");
+
+        input.close();
     }
 }
