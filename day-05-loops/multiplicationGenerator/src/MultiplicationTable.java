e6bad927b95ff928a7b3af70381aae91faf804d2
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-31 17:52:28 +0200

    added the while loop

--- day-05-loops/multiplicationGenerator/src/MultiplicationTable.java
+++ day-05-loops/multiplicationGenerator/src/MultiplicationTable.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args){
-        System.out.println("hello");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("===== Welcome To My Mini Multiplication Calculator🧮 =====");
+        
+        while (true) {
+            System.out.print("Enter the number for the Multiplication table: ");
+            int multicationNumber = input.nextInt();
+
+            for(int i = 1; i <= 11; i++){
+                int result = multicationNumber * i;
+                System.out.println("\nMultiplication for " + multicationNumber + " x " + i + ":");
+                System.out.println(result);
+            }
+
+            //ask if they want to continue or not
+            System.out.print("\nWanna run another table? (yes/no): ");
+            String answer = input.nextLine().trim().toLowerCase();
+
+            if(answer.equals("no")){
+                System.out.println("Thank you for trying my Mini Multiplication Tracker!");
+                break;
+            } else {
+                System.out.println("\n ---- Lets run another table: ");
+            }
+
+        }
+
+            
+        input.close();
     }
 }
