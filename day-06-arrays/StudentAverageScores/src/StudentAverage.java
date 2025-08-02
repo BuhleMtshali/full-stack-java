5f93bfd73458699f9652bbfc2e4f14f9baebe1ab
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-02 13:39:01 +0200

    created the skeleton for the student avarage

--- day-06-arrays/StudentAverageScores/src/StudentAverage.java
+++ day-06-arrays/StudentAverageScores/src/StudentAverage.java
@@ -1,15 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class StudentAverage {
     public static void main(String[] args){
-        int sum = 0;
-        //defining an array with student grades
-        int[] grades = {46, 57, 89, 34, 52, 92, 66, 33, 76, 86};
+        Scanner input = new Scanner(System.in);
 
-        //looping through the array
-        for(int grade : grades){
-            sum += grade;
-        }
-        System.out.println("The total sum is: " + sum);
-        int average = sum / grades.length; 
-        System.out.println("The average: " + average);
+        input.close();
     }
 }
