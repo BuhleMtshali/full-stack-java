df25d3b50f51131bca2b0b6762063e0b94231539
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-02 13:54:44 +0200

    updated the java file

--- day-06-arrays/StudentAverageScores/src/StudentAverage.java
+++ day-06-arrays/StudentAverageScores/src/StudentAverage.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class StudentAverage {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("========= Welcome to My Mini Student Average Checker🍄 ======");
+
+        //starting the while loop
+        System.out.print("How Many student grades do you want to add: ");
+        int maxGrade = input.nextInt();
+
+        int[] grades = new int[maxGrade];
+
+        for(int i = 0; i < maxGrade; i++){
+            System.out.print("Enter grade for student #" + (i + 1) + ":");
+            grades[i] = input.nextInt();
+        }
+
+        System.out.println("\nHere are the grades you entered: ");
+        for(int grade : grades){
+            System.out.println("Grade:" + grade);
+        }
+
 
         input.close();
     }
