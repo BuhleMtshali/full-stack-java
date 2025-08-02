6548d5ccf43ba1c9a9ec18e782fcbeffe33a61d6
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-02 13:27:21 +0200

    updated the student average

--- day-06-arrays/StudentAverageScores/src/StudentAverage.java
+++ day-06-arrays/StudentAverageScores/src/StudentAverage.java
@@ -1,12 +1,15 @@
 public class StudentAverage {
     public static void main(String[] args){
-
+        int sum = 0;
         //defining an array with student grades
         int[] grades = {46, 57, 89, 34, 52, 92, 66, 33, 76, 86};
 
         //looping through the array
         for(int grade : grades){
-            System.out.println(grade);
+            sum += grade;
         }
+        System.out.println("The total sum is: " + sum);
+        int average = sum / grades.length; 
+        System.out.println("The average: " + average);
     }
 }
