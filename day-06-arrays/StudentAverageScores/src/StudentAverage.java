038861b685620f4066d6d7e696a9d82c17bef3a2
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-02 14:30:27 +0200

    added a while loop to my student average checker

--- day-06-arrays/StudentAverageScores/src/StudentAverage.java
+++ day-06-arrays/StudentAverageScores/src/StudentAverage.java
@@ -5,22 +5,44 @@ public class StudentAverage {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("========= Welcome to My Mini Student Average Checker🍄 ======");
+        System.out.println("How to use it: ");
+        System.out.println("1. Enter the number of students, you want to calculate their avarage, then, enter ");
+        System.out.println("2. Next, enter the final grade for each student, and boom, you'll have your average!!!");
 
         //starting the while loop
-        System.out.print("How Many student grades do you want to add: ");
-        int maxGrade = input.nextInt();
+        while (true) {
 
-        int[] grades = new int[maxGrade];
+            System.out.print("How Many student grades do you want to add: ");
+            int maxGrade = input.nextInt();
 
-        for(int i = 0; i < maxGrade; i++){
-            System.out.print("Enter grade for student #" + (i + 1) + ": ");
-            grades[i] = input.nextInt();
+            int[] grades = new int[maxGrade];
+
+            for(int i = 0; i < maxGrade; i++){
+                System.out.print("Enter grade for student #" + (i + 1) + ": ");
+                grades[i] = input.nextInt();
+            }
+
+            System.out.println("\nHere are the grades you entered: ");
+            for(int grade : grades){
+                System.out.println("Grade:" + grade);
+            }
+
+            input.nextInt();
+
+            //asking if they wanna continue with the loop
+            System.out.println("\nWanna run again?");
+            String answer = input.nextLine().trim().toLowerCase();
+
+            if(answer.equals("no")){
+                System.out.println("\nThank you for trying my mini grade checker");
+                break;
+            } else {
+                System.out.println("\nOkay, lets give it another round!!!");
+            }
+
+
         }
-
-        System.out.println("\nHere are the grades you entered: ");
-        for(int grade : grades){
-            System.out.println("Grade:" + grade);
-        }
+        
 
 
         input.close();
