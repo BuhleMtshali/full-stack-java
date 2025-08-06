3cf172bcfcd8639f7f33381db967be6c9bf8d1ad
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-06 23:43:28 +0200

    updated my file

--- day-08-method-overloading/MethodOverloading/src/MethodOverloading.java
+++ day-08-method-overloading/MethodOverloading/src/MethodOverloading.java
@@ -9,12 +9,19 @@ class Greeting{
    public void sayHello(String name){
     System.out.println("Hello " + name);
    }
+   
+   public void sayHello(String scannerName, int age){
+    System.out.println("Hello " + scannerName + " you are currently " + age " years old!");
+    
+   }
 }
 
 
 public class MethodOverloading {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+
+
 
         input.close();
     }
