e83bc84eb287b8cdb5b60e6092f1ba9fcde1301a
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-09 20:06:41 +0200

    updated the bank account

--- day-09-class-objects/BankAccoutClass/src/BankAccount.java
+++ day-09-class-objects/BankAccoutClass/src/BankAccount.java
@@ -56,16 +56,13 @@ public class BankAccount {
 
         
         //START THE WHILE LOOP LOOP FOR VALIDATING USER DETAILS
-        while (!(userName.length() >= 3 && 
-                userAccountNumber.length() >= 5 && 
-                userPin.length() == 5)) {
-                
-            System.out.println("🚫Invalid input, Please try again.");
+        while (true) {    
+            //System.out.println("🚫Invalid input, Please try again.");
             
             System.out.print("Enter the name you want to register your account with(at least 3 characters): ");
             userName = input.nextLine();
 
-            System.out.print("Enter your account number: ");
+            System.out.print("Enter your account number(at least 5-digits): ");
             userAccountNumber = input.nextLine();
 
             System.out.print("Choose an account Type(savings, cheque, credit): ");
@@ -73,6 +70,11 @@ public class BankAccount {
 
             System.out.print("Enter PIN (exactly 5 digit): ");
             userPin = input.nextLine();
+
+            if(userName.length() > 3 && userAccountNumber.length() >= 5 && userAccountNumber != null && userPin.length() == 5){
+                System.out.println("✅ Details entered correctly");
+                break;
+            }
         }
 
         
