c4485c44da0d37bdd0eea2a8444f92467faf0551
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-09 19:08:34 +0200

    updated the bank account class

--- day-09-class-objects/BankAccoutClass/src/BankAccount.java
+++ day-09-class-objects/BankAccoutClass/src/BankAccount.java
@@ -21,12 +21,16 @@ class Bank{
    public void transfer( String recipientName, String accountNumber, double tranferValue ){
     if (this.amount > 0 && this.amount < tranferValue && accountNumber.length() >= 5){
         this.amount -= tranferValue;
+    } else {
+        System.out.println("🚫Insufficient funds to complete the Transaction");
     }
    }
 
    public void payBills(String billName, String billAccountNumber, double billAmount){
     if (this.amount > 0 && this.amount > billAmount){
         this.amount -= billAmount;
+    } else {
+        System.out.println("🚫Insufficient funds to complete the Transaction");
     }
    }
 }
@@ -35,15 +39,24 @@ public class BankAccount {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        //welcome message
-        System.out.println("======= Welcome to JavaBank🏧 =======");
-
         //creating a new object
         Bank bankActions = new Bank();
 
+        //welcome message
+        System.out.println("\n======= Welcome to JavaBank🏧 =======");
+        System.out.println("\nLet's get your details first!");
+
+
+        //get the user details
+        System.out.print("Enter ");
+
+        
+
         //STARTING THE WHILE LOOP
         while (true) {
-            System.out.println("practise loop");
+
+            //theese are our options
+            System.out.println("1. View Account");
 
 
 
