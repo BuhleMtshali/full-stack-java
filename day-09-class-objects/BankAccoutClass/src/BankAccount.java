181b3aaafef93f0d4ec57d0d4e5aedae487a7a92
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-09 18:16:48 +0200

    updated the bankaccount class add added the different methods i want to execute in the main function

--- day-09-class-objects/BankAccoutClass/src/BankAccount.java
+++ day-09-class-objects/BankAccoutClass/src/BankAccount.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
+class Bank{
+   double amount = 1000;
+
+   public void deposit(double addValue){
+    this.amount += addValue; 
+   }
+
+   public void withdraw(double withdrawValue){
+    this.amount -= withdrawValue;
+   }
+}
+
 public class BankAccount {
     public static void main(String[] args){
         System.out.println("hello ");
