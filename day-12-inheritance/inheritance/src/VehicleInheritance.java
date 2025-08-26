f30735e3d57a88caaa2d4717fff1d94f8f20dd18
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-26 13:36:59 +0200

    created a subclass

--- day-12-inheritance/inheritance/src/VehicleInheritance.java
+++ day-12-inheritance/inheritance/src/VehicleInheritance.java
@@ -2,8 +2,18 @@
 class Vehicle{
     String name;
     String model;
+
+    public void drive(){
+        System.out.println("You drive a " + name + " which is a " + model + " vehicle");
+    }
 }
 
+//CHILD CLASS (SUB CLASS) INHERITS FROM VEHICLE
+class miniVehicle extends Vehicle{
+    public void car(){
+        System.out.println("Helo mini you, you drive a " + name + " which is a " + model + " model");
+    }
+}
 
 
 
