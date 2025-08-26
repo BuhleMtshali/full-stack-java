02d787a0a249f08db62f5ee74e913c7a08e9e2d9
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-26 13:31:45 +0200

    updated the parent classs

--- day-12-inheritance/inheritance/src/VehicleInheritance.java
+++ day-12-inheritance/inheritance/src/VehicleInheritance.java
@@ -1,3 +1,12 @@
+//PARENT CLASS (SUPER CLASS)
+class Vehicle{
+    String name;
+    String model;
+}
+
+
+
+
 public class VehicleInheritance {
     public static void main(String[] args){
         System.out.println("hello");
