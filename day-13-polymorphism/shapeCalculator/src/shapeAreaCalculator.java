ca185770b2aedabdeafffd3f02509ab04e2854fd
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-30 12:27:37 +0200

    creayed the first instance

--- day-13-polymorphism/shapeCalculator/src/shapeAreaCalculator.java
+++ day-13-polymorphism/shapeCalculator/src/shapeAreaCalculator.java
@@ -1,13 +1,22 @@
 //RUNTIME EXAMPLE
 class CalculateArea{
+
+    //TWO PARAMETERS
     public double add(double a, double b){
         return a + b;
+    }
+
+    //THREE PARAMETERS
+    public double add(double a, double b, double c){
+        return a + b + c;
     }
 }
 
 
 public class shapeAreaCalculator {
     public static void main(String[] args){
-        System.out.println("polymorphism");
+        CalculateArea calc = new CalculateArea();
+        System.out.println("Example with 2 parameters: " + calc.add(3.5, 5.8));
+        System.out.println("Example with Three parameters: " + calc.add(6.8, 9.9, 12.4));
     }
 }
