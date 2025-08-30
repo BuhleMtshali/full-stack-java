54f5c06d785fb908ae9c4e8c9f6ba4934f4662de
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-30 12:51:02 +0200

    created the runtime

--- day-13-polymorphism/shapeCalculator/src/RuntimeShapeCalculator.java
+++ day-13-polymorphism/shapeCalculator/src/RuntimeShapeCalculator.java
@@ -1,3 +1,15 @@
+class Shape{
+    public void calculate(){
+        System.out.println("original method");
+    }
+}
+
+class shapeOne extends Shape{
+    public void calculate(){
+        System.out.println("this is the second method overiding the first method");
+    }
+}
+
 public class RuntimeShapeCalculator {
     public static void main(String[] args){
         System.out.println("hello");
