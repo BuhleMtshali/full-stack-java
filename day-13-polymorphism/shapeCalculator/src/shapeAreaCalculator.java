6ece474375b03e9ae6dd63e42aa0fe906e27f864
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-30 12:22:15 +0200

    created the class

--- day-13-polymorphism/shapeCalculator/src/shapeAreaCalculator.java
+++ day-13-polymorphism/shapeCalculator/src/shapeAreaCalculator.java
@@ -1,3 +1,11 @@
+//RUNTIME EXAMPLE
+class CalculateArea{
+    public double add(double a, double b){
+        return a + b;
+    }
+}
+
+
 public class shapeAreaCalculator {
     public static void main(String[] args){
         System.out.println("polymorphism");
